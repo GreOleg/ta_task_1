326deee67150c8054d00b1ce9964b21e8cafb247
Oleg Gresik <grek2409@gmail.com>
2022-04-01 11:24:22 +0300

    Added NLog reports

--- PageObjects/MainPageObject.cs
+++ PageObjects/MainPageObject.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NLog;
+using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
 using ta_task_1.WrapperFactory;
 
@@ -7,12 +8,14 @@ namespace ta_task_1.PageObjects
     class MainPageObject
     {
         private IWebDriver driver = BrowserFactory.Driver;
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
 
         [FindsBy(How = How.CssSelector, Using = "a.login")]
         private IWebElement _signInButton { get; set; }
         public void GoToAuthenticationPage()
         {
             _signInButton.Click();
+            _logger.Info($"Click on sign in button => {_signInButton}");
         }
     }
 }
--- PageObjects/Navigation.cs
+++ PageObjects/Navigation.cs
@@ -1,12 +1,15 @@
 ﻿using ta_task_1.WrapperFactory;
+using NLog;
 
 namespace ta_task_1.PageObjects
 {
     class Navigation
-    {
+    {   
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
         public static void GoTo(string url)
         {
             BrowserFactory.Driver.Navigate().GoToUrl(url);
+            _logger.Info($"Opend url => {url}");
         }
     }
 }
